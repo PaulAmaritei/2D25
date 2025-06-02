333571df2ccd87076b740d2df1ad300f91776ef1
PaulAmaritei <amariteipaul@gmail.com>
2025-06-02 09:58:52 -0400

    Update BallScript.cs

--- Assets/BallScript.cs
+++ Assets/BallScript.cs
@@ -1,16 +1,19 @@
 using UnityEngine;
-
 public class BallScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Rigidbody2D rb;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        // Optional: Give initial velocity
+        rb.velocity = new Vector2(2f, 5f); // for example
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        // Add a small random torque when hitting something
+        float randomTorque = Random.Range(-50f, 50f);
+        rb.AddTorque(randomTorque);
     }
 }
